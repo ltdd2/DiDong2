2d9de9a580816fc04f17b2683be55ab0b407de2b
DoMinhVan <43976823+DoMinhVan@users.noreply.github.com>
2019-04-15 23:28:00 +0700

    update màn hình chào

--- CongThucNauAn/app/src/main/java/com/example/congthucnauan/ManHinhChaoActivity.java
+++ CongThucNauAn/app/src/main/java/com/example/congthucnauan/ManHinhChaoActivity.java
@@ -3,11 +3,16 @@ package com.example.congthucnauan;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.congthucnauan.database.Database;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ManHinhChaoActivity extends AppCompatActivity {
     public static Database database;
+    CircleImageView imgIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,10 @@ public class ManHinhChaoActivity extends AppCompatActivity {
             }
         });
         thread.start();
+        //Xoay HInh
+        imgIcon = (CircleImageView) findViewById(R.id.icon);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.xoay);
+        imgIcon.startAnimation(animation);
         //DATABASE
         database = new Database(this, "CongThucNauAn.sqlite", null, 1);
         //Tạo bảng MonAn
