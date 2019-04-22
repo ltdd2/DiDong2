9cc03aeb95124ab39e98642000a6cf7116baac10
DoMinhVan <43976823+DoMinhVan@users.noreply.github.com>
2019-04-22 19:54:27 +0700

    Update ManHinhChaoActivity.java

--- CongThucNauAn/app/src/main/java/com/example/congthucnauan/ManHinhChaoActivity.java
+++ CongThucNauAn/app/src/main/java/com/example/congthucnauan/ManHinhChaoActivity.java
@@ -21,7 +21,7 @@ public class ManHinhChaoActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(5000);
+                    Thread.sleep(3000);
                 } catch (Exception e) {
 
                 } finally {
